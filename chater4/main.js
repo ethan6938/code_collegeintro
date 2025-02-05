efb39f1e5e33e78fb6c53b6628f2af4b1ed93685
ethan6938 <en.hurwitz@gmail.com>
2025-02-05 09:42:54 +0200

    Published chater4

---
+++ chater4/main.js
@@ -0,0 +1,39 @@
+var myPerson = {
+    "name": "Ethan",
+    "age": "16",
+    "favorite Color": "blue"
+};
+
+var myObject = {
+
+
+};
+myObject["myfirstKey"] = "random string"
+myObject["mysecondkey"] = "fale"
+
+var family = [
+    {
+        "relation": "mom",
+        "name": "rayma",
+        "age": " 32"
+    },
+    {
+        "relation": "sister",
+        "name": "devion",
+        "age": "7"
+    },
+]
+
+var game = {
+    "wwe": {
+        "relest": "2024",
+        "favourite person": "aj styles",
+        "best person": "codyrhodes"
+    },
+    "fifa": {
+        "relest": "2025",
+        "favourite person": "kevindebruyne",
+        "best plyer": "messi"
+    },
+
+}

37447ef4e062d84d5d8183de4a38f0bdf53785e2
ethan6938 <en.hurwitz@gmail.com>
2025-02-06 15:08:35 +0200

    Published chapter 7

---
+++ chapter 7/test.js
@@ -0,0 +1,10 @@
+//var colour= prompt("what is your favorite colour?");
+var js= confirm("Do you like javascript?");
+
+//var alerpromt =alert("boom");
+
+if(js) {
+   console.log("that is cool.")
+} else{
+    console.log("ya... me neither")
+};

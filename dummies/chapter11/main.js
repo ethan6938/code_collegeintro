779a7a898e4865dbc60631f278a7cba12abcd8f4
ethan6938 <en.hurwitz@gmail.com>
2025-02-11 17:09:15 +0200

    dummies chapter 11

---
+++ dummies/chapter11/main.js
@@ -0,0 +1,16 @@
+var parents = ["father","mother"];
+var firends =["ethann","grant"];
+var myPerson = ["Ethan",16,true];
+var family = parents.concat(firends,myPerson);
+console.log(family.reverse());
+
+var myDog = "zack";
+var myCat = "sandy";
+var pets = [myDog,myCat];
+
+var array = [1,2,3];
+array.unshift("one");
+array.shift();
+
+let longArray = [1,2,3,4,5,6,7,8,9];
+longArray.push("ethan");
